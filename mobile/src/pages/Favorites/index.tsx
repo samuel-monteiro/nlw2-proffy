2e7d552d6f154b47f3fa1d18a3c7e1fa8c78e860
samuel-monteiro <60399878+samuel-monteiro@users.noreply.github.com>
2020-10-09 11:18:47 -0300

    Add files via upload

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,55 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+
+import PageHeader from '../../componentes/PageHeader';
+import TeacherItem, { Teacher } from '../../componentes/TeacherItem';
+
+import styles from './styles';
+
+
+function Favorites() {
+    const [favorites, setFavorites] = useState([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response);
+
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
+
+    return (
+        <View style={styles.container} >
+            <PageHeader title="Meus Proffs disponíveis" />
+
+            <ScrollView
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16,
+                }}
+            >
+                {favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem 
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited={true}
+                        />
+                    )
+                })}
+            </ScrollView>
+        </View>
+    );
+}
+
+export default Favorites;
